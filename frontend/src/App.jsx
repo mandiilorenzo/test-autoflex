67b51838ee0a033becad968b908cb5e5738460f2
mandilorenzo <amanda.lorenzo@hotmail.com>
2026-02-11 16:08:24 -0300

    feat: complete professional CRUD for raw materials (RF006) with custom ConfirmDialog

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RawMaterials from './pages/RawMaterials';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/raw-materials" element={<RawMaterials />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/raw-materials" element={<RawMaterials />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
