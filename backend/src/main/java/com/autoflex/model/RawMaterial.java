2714d6b2711c122b34134410d5bbb5af161eed58
mandilorenzo <amanda.lorenzo@hotmail.com>
2026-03-01 17:41:07 -0300

    refactor(backend): move entities to correct package path

--- backend/src/main/java/com/autoflex/model/RawMaterial.java
+++ backend/src/main/java/com/autoflex/model/RawMaterial.java
@@ -6,12 +6,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "products")
-public class Product extends PanacheEntity {
+@Table(name = "raw_materials")
+public class RawMaterial extends PanacheEntity {
 
     @Column(nullable = false)
     public String name;
 
-    @Column(nullable = false)
-    public Double price;
+    @Column(name = "stock_quantity", nullable = false)
+    public Double stockQuantity;
 }
